fe7da7190998fed3f74d5eda41a8fd4de91c0a4f
7v7kgr6m2c-svg <7v7kgr6m2c@privaterelay.appleid.com>
2026-08-12 23:41:27 +0800

    Update Firebase SDK version and storage bucket URL

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA40NWznF4c7Gz2FNzVTCyse_Tm9gp-1n0",
-    authDomain: "trackandwave.com",
-    projectId: "athletes-record",
-    storageBucket: "athletes-record.appspot.com",
-    messagingSenderId: "259439207122",
-    appId: "1:259439207122:web:26d6211ac1379c936e030b",
-    measurementId: "G-F3ZH9YN0VT"
+  apiKey: "AIzaSyA40NWznF4c7Gz2FNzVTCyse_Tm9gp-1n0",
+  authDomain: "trackandwave.com",
+  projectId: "athletes-record",
+  storageBucket: "athletes-record.firebasestorage.app",
+  messagingSenderId: "259439207122",
+  appId: "1:259439207122:web:26d6211ac1379c936e030b",
+  measurementId: "G-F3ZH9YN0VT"
 };
 
 const app = initializeApp(firebaseConfig);
